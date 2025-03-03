8accfd7dabfe8fb615c8a1e1db0828c54856537f
polet2 <1050883735@qq.com>
2025-03-04 01:39:19 +0800

    Exercise #5

--- SliceOHeaven/SliceOHeaven.java
+++ SliceOHeaven/SliceOHeaven.java
@@ -1,23 +1,21 @@
-import java.util.List;
-
 public class SliceOHeaven {
     private String storeName;
-    @SuppressWarnings("unused")
     private String storeAddress;
     private String storeEmail;
     private String storePhone;
-    private List<String> storeMenu;
-    private List<String> pizzaIngredients;
+    private String storeMenu;
+    private String pizzaIngredients;
     private double pizzaPrice;
-    private List<String> sides;
-    private List<String> drinks;
-    private int orderID;
+    private String sides;
+    private String drinks;
+    private String orderID;
     private double orderTotal;
-    private static final String DEF_ORDER_ID = "DEF-SOH-099";
-    private static final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
-    private static final double DEF_ORDER_TOTAL = 15.00;
 
-    public Pizzeria() {
+    private final String DEF_ORDER_ID = "DEF-SOH-099";
+    private final String DEF_PIZZA_INGREDIENTS = "Mozzarella Cheese";
+    private final double DEF_ORDER_TOTAL = 15.00;
+
+    public SliceOHeaven() {
         this.orderID = DEF_ORDER_ID;
         this.pizzaIngredients = DEF_PIZZA_INGREDIENTS;
         this.orderTotal = DEF_ORDER_TOTAL;
@@ -25,12 +23,10 @@ public class SliceOHeaven {
         this.drinks = "Coke";
     }
 
-    public Pizzeria(String orderID, String pizzaIngredients, double orderTotal) {
+    public SliceOHeaven(String orderID, String pizzaIngredients, double orderTotal) {
         this.orderID = orderID;
         this.pizzaIngredients = pizzaIngredients;
         this.orderTotal = orderTotal;
-        this.sides = "Garlic Bread";
-        this.drinks = "Coke";
     }
 
     public String getOrderID() {
@@ -49,76 +45,62 @@ public class SliceOHeaven {
         this.orderTotal = orderTotal;
     }
 
-    public String getPizzaIngredients() {
-        return pizzaIngredients;
+    public void takeOrder() {
+        System.out.println("Order taken successfully!");
     }
 
-    public void setPizzaIngredients(String pizzaIngredients) {
-        this.pizzaIngredients = pizzaIngredients;
-    }
-
-    public String getSides() {
-        return sides;
-    }
-
-    public void setSides(String sides) {
-        this.sides = sides;
-    }
-
-    public String getDrinks() {
-        return drinks;
-    }
-
-    public void setDrinks(String drinks) {
-        this.drinks = drinks;
-    }
-
-    public SliceOHeaven(String storeName, String storeAddress, String storeEmail, String storePhone, List<String> storeMenu) {
-        this.storeName = storeName;
-        this.storeAddress = storeAddress;
-        this.storeEmail = storeEmail;
-        this.storePhone = storePhone;
-        this.storeMenu = storeMenu;
-        this.orderID = 0;
-        this.orderTotal = 0.0;
-    }
-    public void takeOrder(List<String> pizzaIngredients, List<String> sides, List<String> drinks) {
-        this.pizzaIngredients = pizzaIngredients;
-        this.sides = sides;
-        this.drinks = drinks;
-        this.orderID++;
-        this.orderTotal = calculateOrderTotal();
-    }
-    public String makePizza() {
-        return "Making pizza with ingredients: " + String.join(", ", pizzaIngredients);
+    public void makePizza() {
+        System.out.println("Pizza is being made with ingredients: " + pizzaIngredients);
     }
 
     private void printReceipt() {
-        System.out.println("Order ID: " + orderID);
-        System.out.println("Pizza Ingredients: " + pizzaIngredients);
-        System.out.println("Order Total: $" + orderTotal);
-        System.out.println("Sides: " + sides);
-        System.out.println("Drinks: " + drinks);
+        System.out.println("Receipt for Order ID: " + orderID);
+        System.out.println("Total Amount: $" + orderTotal);
     }
 
-    public void showReceipt() {
-        printReceipt();
+    public void processCardPayment(String cardNumber, String expiryDate, int cvv) {
+        int cardLength = cardNumber.length();
+        if (cardLength == 14) {
+            System.out.println("Card accepted");
+        } else {
+            System.out.println("Invalid card");
+            return;
+        }
+
+        int firstCardDigit = Integer.parseInt(cardNumber.substring(0, 1));
+        System.out.println("First digit of the card: " + firstCardDigit);
+
+        String blacklistedNumber = "12345678901234";
+        if (cardNumber.equals(blacklistedNumber)) {
+            System.out.println("Card is blacklisted. Please use another card");
+            return;
+        }
+
+        int lastFourDigits = Integer.parseInt(cardNumber.substring(cardNumber.length() - 4));
+        System.out.println("Last four digits of the card: " + lastFourDigits);
+
+        String cardNumberToDisplay = cardNumber.charAt(0) +
+                                    "*".repeat(cardNumber.length() - 5) +
+                                    cardNumber.substring(cardNumber.length() - 4);
+        System.out.println("Card number to display: " + cardNumberToDisplay);
     }
-    
-    private double calculateOrderTotal() {
-        // Assuming a fixed price for simplicity
-        double pizzaPrice = 15.0;
-        double sidesPrice = sides.size() * 5.0;
-        double drinksPrice = drinks.size() * 2.0;
-        return pizzaPrice + sidesPrice + drinksPrice;
+
+    public void specialOfTheDay(String pizzaOfTheDay, String sideOfTheDay, double specialPrice) {
+        StringBuilder special = new StringBuilder();
+        special.append("Today's Special:\n");
+        special.append("Pizza: ").append(pizzaOfTheDay).append("\n");
+        special.append("Side: ").append(sideOfTheDay).append("\n");
+        special.append("Special Price: $").append(specialPrice).append("\n");
+        System.out.println(special.toString());
     }
 
     public static void main(String[] args) {
-        List<String> storeMenu = List.of("Margherita", "Pepperoni", "Veggie");
-        SliceOHeaven pizzeria = new SliceOHeaven("Slice-o-Heaven", "123 Pizza St.", "contact@sliceoheaven.com", "123-456-7890", storeMenu);
+        SliceOHeaven pizzaOrder = new SliceOHeaven();
+        pizzaOrder.takeOrder();
+        pizzaOrder.makePizza();
+        pizzaOrder.printReceipt();
 
-        pizzeria.takeOrder(List.of("Cheese", "Tomato", "Basil"), List.of("Garlic Bread"), List.of("Coke"));
-        System.out.println(pizzeria.makePizza());
-        System.out.println(pizzeria.showReceipt());
+        pizzaOrder.processCardPayment("12345678901234", "12/25", 123);
+        pizzaOrder.specialOfTheDay("Pepperoni Pizza", "Garlic Bread", 12.99);
     }
 }
